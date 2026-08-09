9b79501f5d2c1ff84eb262a27cd3bc1e3ec5b33e
JuanCho <yesid.eng15@gmail.com>
2026-08-09 09:15:14 -0500

    090820261

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // Cambiar este dominio al publicar el backend.
-  apiUrl: 'https://api.example.com/api',
-  backendUrl: 'https://api.example.com',
+  apiUrl: 'https://pgic.iestpchojata.edu.pe/api',
+  backendUrl: 'https://pgic.iestpchojata.edu.pe',
 };
